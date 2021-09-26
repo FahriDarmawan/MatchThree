4506d50dfe9cb413b62527bf43735dc76b7aa146
FahriDarmawan <44422245+FahriDarmawan@users.noreply.github.com>
2021-09-26 11:21:39 +0700

    MatchThree

---
+++ Match Three/Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGameOver : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public void PlayAgain()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+				Application.Quit();
+        #endif
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
